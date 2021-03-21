99a86168f49eee8799286524b0d47b038a1fb2ae
baojingh <baojingh@163.com>
2021-03-21 22:14:45 +0800

    add: bean factory registry

---
+++ src/main/java/com/bd/mspring/beans/factory/BeanFactory1.java
@@ -0,0 +1,22 @@
+package com.bd.mspring.beans.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: baojing.he
+ * @Date: 2021-03-20 22:12
+ * @Description:
+ */
+public class BeanFactory1 {
+    private Map<String, Object> beanMap = new HashMap<String, Object>();
+
+    public void registerBean(String name, Object bean) {
+        beanMap.put(name, bean);
+    }
+
+    public Object getBean(String name) {
+        Object o = beanMap.get(name);
+        return o;
+    }
+}
